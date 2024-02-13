6c06cd3d0ae4ee9e443cb7964f5fbc8875a70f9a
motxx <motohiko.ave@gmail.com>
2024-02-13 18:56:00 +0900

    feat: apply honox

--- build.ts
+++
@@ -1,6 +0,0 @@
-import fs from "node:fs/promises";
-import { toSSG } from "hono/ssg";
-import app from "./src/index";
-
-toSSG(app, fs);
-fs.cp("src/assets/", "static/", { recursive: true });
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
-import build from "@hono/vite-ssg";
-import devServer from "@hono/vite-dev-server";
+import ssg from "@hono/vite-ssg";
+import mdx from "@mdx-js/rollup";
+import honox from "honox/vite";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    build(),
-    devServer({
-      entry: "src/index.tsx",
-    }),
-  ],
+const entry = "./app/server.ts";
+
+export default defineConfig(() => {
+  return {
+    plugins: [
+      honox(),
+      ssg({ entry }),
+      mdx({
+        jsxImportSource: "hono/jsx",
+        remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+      }),
+    ],
+  };
 });
